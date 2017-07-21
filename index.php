4faa8c1a4d94bcf20fd89c4703642c0a5ad637c9
Alabi Akpobome Emmanuel <bobslegend795@gmail.com>
2017-07-21 04:09:33 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--Style sheet-->
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="div1">
+        <div id="header" class="flex-container">
+            <div class="brand-name item-1">
+                <strong>ALABS</strong>
+            </div>
+            <div class="links item-2">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="button">
+            Parallex Website
+        </div>
+    </div>
+    <div id="cher1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, fuga.
+    </div>
+    <div id="div2"></div>
+    <div id="cher2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid debitis non quas aperiam, illum facilis facere in enim, odit doloremque nobis numquam officiis veniam magnam repudiandae repellendus mollitia. Architecto, voluptatem?
+    </div>
+    <div id="div3"></div>
+    <div id="cher3">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium autem facere, totam ea deserunt quia, quos adipisci laudantium! Totam facere enim earum quas aliquid consequuntur harum, explicabo autem cum quasi voluptas, praesentium error et eos delectus deserunt ad blanditiis modi cumque quae minima temporibus illum debitis animi. Ut enim, maiores neque quis aspernatur modi eos quia dolor, odio fugiat quod dignissimos error amet suscipit odit illum. Dignissimos repellat fugit aliquam, ab quaerat deleniti illo culpa, aspernatur in iure odit nisi odio obcaecati eum perspiciatis vel consequatur quod quae reprehenderit. Earum quibusdam nemo praesentium, quisquam placeat voluptates consectetur, iste corrupti provident sint veritatis, ad nesciunt! Reiciendis corporis harum nostrum modi placeat porro quos omnis dolore maxime excepturi debitis culpa eum veritatis, asperiores veniam rem possimus labore fugiat, deserunt consequuntur, eligendi sequi mollitia cum magnam! Quis doloremque quo magni, ex maxime cumque, doloribus nihil veniam, similique itaque laborum obcaecati beatae veritatis ab reiciendis repellendus officia eligendi fugiat dicta magnam alias eius. Ipsam fugiat hic inventore. Quis quas, sapiente aperiam officia adipisci, delectus distinctio quos consectetur voluptas provident fugit reprehenderit illo qui facilis aspernatur laborum vero voluptate hic quia aliquam. Quaerat ad sed, quasi, quia architecto nesciunt nulla minima, incidunt dicta molestiae in.
+    </div>
+    <div id="div4"></div>
+</body>
+</html>
